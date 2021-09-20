4e72f53541d3415414903be510478b6fb3e38231
AlekzDev <alex.vargas87@gmail.com>
2021-09-19 21:02:56 -0500

    Primer commit

---
+++ src/test/java/net/alekz/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.alekz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
