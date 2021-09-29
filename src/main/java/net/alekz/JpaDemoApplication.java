9316f358d2c9159179b65b98d7e31ba884ed1094
alekzdev <alex.vargas87@gmail.com>
2021-09-28 23:27:29 -0500

    Ejericio de insert y findById

--- src/main/java/net/alekz/JpaDemoApplication.java
+++ src/main/java/net/alekz/JpaDemoApplication.java
@@ -1,19 +1,40 @@
 package net.alekz;
 
+import net.alekz.model.Categoria;
+import net.alekz.repository.CategoriasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class JpaDemoApplication  implements CommandLineRunner {
+
+	@Autowired
+	private CategoriasRepository categoriasRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
 	}
 
-
 	@Override
 	public void run(String... args) throws Exception {
+		//guardar();
+		System.out.println(buscarporid(1));
+	}
 
+	public void guardar(){
+		Categoria categoria = new Categoria();
+		categoria.setNombre("Finanzas");
+		categoria.setDescripcion("Trabajos relacionados con fiananzas y contabilidad");
+		categoriasRepository.save(categoria);
+		System.out.println(categoria);
+	}
+
+	public Categoria buscarporid(Integer id){
+		Optional<Categoria> categoriaOp = categoriasRepository.findById(id);
+		return categoriaOp.orElse(null);
 	}
 }
--- src/main/java/net/alekz/model/Vacante.java
+++ src/main/java/net/alekz/model/Vacante.java
@@ -3,12 +3,12 @@ package net.alekz.model;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity
-@Table(name="Vacante")
+//@Entity
+//@Table(name="Vacante")
 public class Vacante {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@Id
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
