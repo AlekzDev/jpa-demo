c345a7d72a07d691efb3ab6e6368d09552418efd
alekzdev <alex.vargas87@gmail.com>
2021-10-20 22:50:51 -0500

    Ejericio de find, save

--- src/main/java/net/alekz/JpaDemoApplication.java
+++ src/main/java/net/alekz/JpaDemoApplication.java
@@ -2,11 +2,12 @@ package net.alekz;
 
 import net.alekz.model.Categoria;
 import net.alekz.repository.CategoriasRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -30,7 +31,11 @@ public class JpaDemoApplication  implements CommandLineRunner {
 		//System.out.println(buscarporid(2));
 		//eliminar(1);
 		//conteo();
-		eliminarTodos();
+		//eliminarTodos();
+		//encontrarPorIds();
+		//buscarTodas();
+		//existeid(20);
+		guardarTodos();
 	}
 
 	public void guardar(){
@@ -63,5 +68,50 @@ public class JpaDemoApplication  implements CommandLineRunner {
 
 	public void eliminarTodos(){
 		categoriasRepository.deleteAll();
+	}
+
+	public void encontrarPorIds(){
+		List<Integer> ids = new LinkedList<Integer>();
+		ids.add(1);
+		ids.add(4);
+		ids.add(10);
+		Iterable<Categoria> categorias = categoriasRepository.findAllById(ids);
+		categorias.forEach(System.out::println);
+	}
+
+	public void buscarTodas(){
+		Iterable<Categoria> categorias = categoriasRepository.findAll();
+		categorias.forEach(System.out::println);
+	}
+
+	public void existeid(Integer id){
+		if(categoriasRepository.existsById(id))
+			System.out.println("El registro existe: " + buscarporid(id));
+		else
+			System.out.println("El registro no existe");
+	}
+
+	public void guardarTodos(){
+		List<Categoria> categorias = new LinkedList<Categoria>();
+		Categoria cat1 = new Categoria();
+		cat1.setId(16);
+		cat1.setNombre("Materia optativa 1");
+		cat1.setDescripcion("Materia optativa 1");
+
+		Categoria cat2 = new Categoria();
+		cat2.setId(17);
+		cat2.setNombre("Materia optativa 2");
+		cat2.setDescripcion("Materia optativa 2");
+
+		Categoria cat3 = new Categoria();
+		cat3.setId(18);
+		cat3.setNombre("Materia optativa 3");
+		cat3.setDescripcion("Materia optativa 3");
+
+		categorias.add(cat1);
+		categorias.add(cat2);
+		categorias.add(cat3);
+
+		categoriasRepository.saveAll(categorias);
 	}
 }
